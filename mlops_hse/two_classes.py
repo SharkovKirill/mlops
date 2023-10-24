df598479a37eaa53ffebb5bbac355684416591f4
KirillSharkov <kirill.office75@gmail.com>
2023-10-25 00:22:13 +0300

    added good api

--- mlops_hse/two_classes.py
+++ mlops_hse/two_classes.py
@@ -1,6 +1,8 @@
 from catboost import CatBoostClassifier
 from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
+from typing import Dict, List
+import numpy as np
 
 
 class MyKeyError(KeyError):
@@ -34,44 +36,79 @@ class AllModels(object):
             "cb": CatBoostClassifier,
             "rf": RandomForestClassifier,
         }
-        self.__models = {}
-        self.__names_fitted_models = []
+        self.__models: Dict[Model] = {}
+        self.__names_fitted_models: List[str] = []
 
-    def get_avaliable_model_types(self):
-        return self.__available_model_types
-
-    def get_models(self, only_fitted=False):
-        if only_fitted:
-            return self.__names_fitted_models
+    def get_avaliable_model_types(self, show=False):
+        if show:
+            return [
+                {"model_name": key, "model_type": str(value)}
+                for (key, value) in self.__available_model_types.items()
+            ]
         else:
-            return list(self.__models.keys())
+            return self.__available_model_types
 
-    def delete_model(self, user_model_name):
-        try:
-            del self.__models[user_model_name]
-            self.__names_fitted_models.remove(user_model_name)
-        except KeyError:
-            raise MyKeyError("Модели с таким названием не существует")
+    def get_models(
+        self, only_fitted: bool = False, name_models: str | None = None
+    ):
+        if name_models is not None and only_fitted:
+            if name_models in self.__names_fitted_models:
+                name_models = [name_models]
+            else:
+                raise MyKeyError(
+                    "Модель с таким именем не найдена или не обучена"
+                )
+        elif name_models is not None:
+            if name_models in list(self.__models.keys()):
+                name_models = [name_models]
+            else:
+                raise MyKeyError("Модель с таким именем не найдена")
+        else:
+            print("cr not name models")
+            if only_fitted:
+                print("зашло")
+                name_models = self.__names_fitted_models
+            else:
+                name_models = list(self.__models.keys())
+        return [
+            {
+                "user_model_name": user_model_name,
+                "type_model": self.__models[user_model_name].type_model,
+                "params": self.get_params(user_model_name),
+                "fitted": self.__models[user_model_name].fiited,
+            }
+            for user_model_name in name_models
+        ]
 
-    def init_new_model(self, type_model, user_model_name: str, params={}):
+    def init_new_model(
+        self, type_model: str, user_model_name: str, params: Dict = {}
+    ):
         if user_model_name in self.__models.keys():
             raise MyAlreadyExitsError("Модель с таким именем уже существует")
         if type_model not in self.__available_model_types.keys():
             raise MyKeyError("Выбранной модели нет в списке доступных")
         self.__models[user_model_name] = Model(
             self.__available_model_types[type_model],
+            type_model,
             user_model_name,
             params=params,
         )
+        return {
+            "user_model_name": user_model_name,
+            "type_model": type_model,
+            "params": params,
+            "fitted": False,
+        }
 
-    def model_fit(self, X, y, user_model_name):
+    def model_fit(self, X: np.array, y: np.array, user_model_name: str):
         try:
             self.__models[user_model_name].fit(X, y)
             self.__names_fitted_models.append(user_model_name)
+            self.fitted = True
         except KeyError:
             raise MyKeyError("Модель с таким имененем не найдена")
 
-    def model_predict(self, X, user_model_name):
+    def model_predict(self, X: np.array, user_model_name):
         if user_model_name in self.__names_fitted_models:
             return self.__models[user_model_name].predict(X)
         else:
@@ -82,6 +119,14 @@ class AllModels(object):
     def get_params(self, user_model_name, all: bool = False):
         return self.__models[user_model_name].get_params(all)
 
+    def delete_model(self, user_model_name: str):
+        try:
+            del self.__models[user_model_name]
+            if user_model_name in self.__names_fitted_models:
+                self.__names_fitted_models.remove(user_model_name)
+        except KeyError:
+            raise MyKeyError("Модели с таким названием не существует")
+
 
 class Model:
     getting_params_func_names = {
@@ -89,21 +134,27 @@ class Model:
         RandomForestClassifier: RandomForestClassifier.get_params,
     }
 
-    def __init__(self, base_model, user_model_name: str, params) -> None:
-        self.params = params
-        self.user_model_name = user_model_name
+    def __init__(
+        self, base_model, type_model, user_model_name: str, params
+    ) -> None:
+        self.type_model: str = type_model
+        self.params: Dict = params
+        self.user_model_name: str = user_model_name
+        self.fiited: bool = False
+        print(params)
         try:
             self.base_model = base_model(**self.params)
         except TypeError:
             raise MyTypeError("Переданы неправильные гиперпараметры модели")
 
-    def fit(self, X, y):
+    def fit(self, X: np.array, y: np.array):
         try:
             self.base_model.fit(X=X, y=y)
+            self.fiited = True
         except:
             raise InvalidData("Некорректные данные для обучения")
 
-    def predict(self, X):
+    def predict(self, X: np.array):
         try:
             return self.base_model.predict(X)
         except:
@@ -122,6 +173,7 @@ df = pd.DataFrame({"col1": [1, 2, 4], "col2": [1, 2, 4], "target": [4, 5, 6]})
 print(df)
 
 allm = AllModels()
+
 
 print(allm.get_avaliable_model_types())
 print(allm.get_models())
